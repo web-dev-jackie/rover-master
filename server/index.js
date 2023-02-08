80bfa994cf9a662fde888d47c67cc801ee9a7a4c
Jackie <heatleyj7@icloud.com>
2023-02-08 00:08:02 -0500

    Try again

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const express      = require('express');
-const path         = require('path');
+const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
-// var cors = require('cors')
+var cors = require('cors')
 
 require('./config/environment');
 require('./database');
 
-const routes          = require('./routes/index');
-const configPassport  = require('./passport/config');
+const routes = require('./routes/index');
+const configPassport = require('./passport/config');
 
-const assetFolder  = path.resolve(__dirname, '../dist/');
-const port         = process.env.PORT;
-const app          = express();
+const assetFolder = path.resolve(__dirname, '../dist/');
+const port = process.env.PORT;
+const app = express();
 
 app.use(express.static(assetFolder));
 app.use(bodyParser.json());
@@ -20,6 +20,6 @@ configPassport(app, express);
 
 app.use('/', routes());
 
-// app.use(cors());
+app.use(cors({ origin: ["http://localhost:3000"]}));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
